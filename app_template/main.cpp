93f59945b766f90a64010e0dba04d17c4b577e3d
Greg <gjhamlin@gmail.com>
2025-02-23 16:59:30 -0500

    refactoring

---
+++ app_template/main.cpp
@@ -0,0 +1,13 @@
+#include "mssm.h"
+
+int main()
+{
+    println("Type something: ");
+
+    string response = readLine();
+
+    println("You typed: {}", response);
+
+    return 0;
+}
+
--- libraries/cmake/AppAssets.cmake
+++ libraries/cmake/AppAssets.cmake
@@ -1,75 +1,116 @@
 include(${CMAKE_SOURCE_DIR}/../libraries/cmake/util.cmake)
 
 # Assets for all platforms
-# set(SHARED_STATIC_ASSETS ${PROJECT_SOURCE_DIR}/assets/fonts/Manrope.ttf)
+# set(STATIC_ASSETS ${PROJECT_SOURCE_DIR}/assets/fonts/Manrope.ttf)
+
+message("PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
+message("CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
+message("CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}")
+
+if(EXISTS "${PROJECT_SOURCE_DIR}/assets")
+    # look for project level assets
+    set(ASSETS_SOURCE_FOLDER "${PROJECT_SOURCE_DIR}/assets")
+else()
+    # revert to mssm_code assets
+    set(ASSETS_SOURCE_FOLDER "${CMAKE_SOURCE_DIR}/../assets")
+endif()
+
+if(EXISTS "${PROJECT_SOURCE_DIR}/assets/vulkan")
+    # look for project level assets
+    set(ASSETS_VULKAN_SOURCE_FOLDER "${PROJECT_SOURCE_DIR}/assets/vulkan")
+    set(ASSETS_VULKAN_PARENT_FOLDER "${PROJECT_SOURCE_DIR}/assets")
+else()
+    # revert to mssm_code assets
+    set(ASSETS_VULKAN_SOURCE_FOLDER "${CMAKE_SOURCE_DIR}/../assets/vulkan")
+    set(ASSETS_VULKAN_PARENT_FOLDER "${CMAKE_SOURCE_DIR}/../assets")
+endif()
+
+if(EXISTS "${PROJECT_SOURCE_DIR}/assets/icons")
+    # look for project level icons
+    set(ASSETS_ICONS_SOURCE_FOLDER "${PROJECT_SOURCE_DIR}/assets/icons")
+else()
+    # revert to mssm_code icons
+    set(ASSETS_ICONS_SOURCE_FOLDER "${CMAKE_SOURCE_DIR}/../assets/icons")
+endif()
+
+
+if(EXISTS "${PROJECT_SOURCE_DIR}/packaging")
+    # look for project level packaging info
+    set(PROJECT_PACKAGING_SOURCE_FOLDER "${PROJECT_SOURCE_DIR}/packaging")
+else()
+    # revert to mssm_code packaging info
+    set(PROJECT_PACKAGING_SOURCE_FOLDER "${CMAKE_SOURCE_DIR}/../packaging")
+endif()
 
 # Platform specific static assets
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
   target_sources(${PROJECT_NAME} PUBLIC
-    ${SHARED_STATIC_ASSETS}
-    ${PROJECT_SOURCE_DIR}/assets/icons/icon.ico
-    ${PROJECT_SOURCE_DIR}/packaging/Manifests/app.rc
-    ${PROJECT_SOURCE_DIR}/packaging/Manifests/App.manifest)
+    ${STATIC_ASSETS}
+    ${ASSETS_ICONS_SOURCE_FOLDER}/icon.ico
+    ${PROJECT_PACKAGING_SOURCE_FOLDER}/Manifests/app.rc
+    ${PROJECT_PACKAGING_SOURCE_FOLDER}/Manifests/App.manifest)
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
   set(MACOSX_STATIC_ASSETS
-    ${SHARED_STATIC_ASSETS}
-    ${PROJECT_SOURCE_DIR}/assets/icons/icon.icns)
+    ${STATIC_ASSETS}
+    ${ASSETS_ICONS_SOURCE_FOLDER}/icons/icon.icns)
   target_sources(${PROJECT_NAME} PUBLIC ${MACOSX_STATIC_ASSETS} ${ASSETS})
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  target_sources(${PROJECT_NAME} PRIVATE ${SHARED_STATIC_ASSETS})
+  target_sources(${PROJECT_NAME} PRIVATE ${STATIC_ASSETS})
 endif ()
 
-# build list of assets
-set(ASSETS_SOURCE_FOLDER "${CMAKE_CURRENT_SOURCE_DIR}/assets")
-file(GLOB_RECURSE ASSETS "${ASSETS_SOURCE_FOLDER}/*")
-filter_regex(EXCLUDE "\\.DS_Store" ASSETS ${ASSETS})
+if(EXISTS "${ASSETS_SOURCE_FOLDER}")
 
-foreach (FILE ${ASSETS})
-    # make sure these files don't actually get compiled
-    # (wavefront obj mesh files were being treated as compiled files to be linked)
-    set_source_files_properties(${FILE} PROPERTIES HEADER_FILE_ONLY TRUE)
-endforeach ()
+    # build list of assets
+    file(GLOB_RECURSE ASSETS "${ASSETS_SOURCE_FOLDER}/*")
+    filter_regex(EXCLUDE "\\.DS_Store" ASSETS ${ASSETS})
 
-target_sources(${PROJECT_NAME} PUBLIC ${ASSETS})
+    foreach (FILE ${ASSETS})
+        # make sure these files don't actually get compiled
+        # (wavefront obj mesh files were being treated as compiled files to be linked)
+        set_source_files_properties(${FILE} PROPERTIES HEADER_FILE_ONLY TRUE)
+    endforeach ()
 
-# loop over assets and make sure they get copied to the executable location
-foreach (FILE ${ASSETS})
+    target_sources(${PROJECT_NAME} PUBLIC ${ASSETS})
 
-    # message("Procesing file: ${FILE}")
+    # loop over assets and make sure they get copied to the executable location
+    foreach (FILE ${ASSETS})
 
-    # Get the relative path from the data-folder to the particular file.
-    file(RELATIVE_PATH NEW_FILE "${ASSETS_SOURCE_FOLDER}" ${FILE})
+        # message("Procesing file: ${FILE}")
 
-    # Get the relative path to the file.
-    get_filename_component(NEW_FILE_PATH ${NEW_FILE} DIRECTORY)
+        # Get the relative path from the data-folder to the particular file.
+        file(RELATIVE_PATH NEW_FILE "${ASSETS_SOURCE_FOLDER}" ${FILE})
 
-    # MAC:  Set it's location inside the app package (under Resources).
-    set_property(SOURCE ${FILE} PROPERTY MACOSX_PACKAGE_LOCATION "Resources/${NEW_FILE_PATH}")
+        # Get the relative path to the file.
+        get_filename_component(NEW_FILE_PATH ${NEW_FILE} DIRECTORY)
 
-    install(FILES ${FILE} DESTINATION ${CMAKE_INSTALL_BINDIR}/assets/${NEW_FILE_PATH})
+        # MAC:  Set it's location inside the app package (under Resources).
+        set_property(SOURCE ${FILE} PROPERTY MACOSX_PACKAGE_LOCATION "Resources/${NEW_FILE_PATH}")
 
-    # message("NEWFILE PATH: ${NEW_FILE_PATH}")
+        install(FILES ${FILE} DESTINATION ${CMAKE_INSTALL_BINDIR}/assets/${NEW_FILE_PATH})
 
-    if(NOT APPLE)
-        add_custom_command(
-            TARGET ${PROJECT_NAME} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets"
-            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${FILE}" "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets/${NEW_FILE}"
-        )
+        # message("NEWFILE PATH: ${NEW_FILE_PATH}")
 
-    endif()
+        if(NOT APPLE)
+            add_custom_command(
+                TARGET ${PROJECT_NAME} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets"
+                COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${FILE}" "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets/${NEW_FILE}"
+            )
 
-    # message("Assets/${NEW_FILE} FILE ${FILE}")
-    # Make sure it shows up in the IDE Assets folder
-    source_group("Assets/${NEW_FILE_PATH}" FILES "${FILE}")
+        endif()
 
-endforeach ()
+        # message("Assets/${NEW_FILE} FILE ${FILE}")
+        # Make sure it shows up in the IDE Assets folder
+        source_group("Assets/${NEW_FILE_PATH}" FILES "${FILE}")
+
+    endforeach ()
+
+endif()
 
 if ("vulkan" IN_LIST LIBRARIES)
     message("Vulkan library being used: setting up shader compilation...")
 
-    set(ASSETS_SOURCE_FOLDER "${CMAKE_CURRENT_SOURCE_DIR}/assets")
-    file(GLOB_RECURSE GLSL_SOURCE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/assets/vulkan/*.glsl")
+    file(GLOB_RECURSE GLSL_SOURCE_FILES "${ASSETS_VULKAN_SOURCE_FOLDER}/*.glsl")
 
     include(${CMAKE_SOURCE_DIR}/../libraries/cmake/glsl-shaders.cmake)
 
@@ -83,7 +124,7 @@ if ("vulkan" IN_LIST LIBRARIES)
         message("Processing GLSL file: ${FILE}")
 
         # Get the relative path from the data-folder to the particular file.
-        file(RELATIVE_PATH NEW_FILE "${ASSETS_SOURCE_FOLDER}" ${FILE})
+        file(RELATIVE_PATH NEW_FILE "${ASSETS_VULKAN_PARENT_FOLDER}" ${FILE})
 
         # Get the relative path to the file.
         get_filename_component(NEW_FILE_PATH ${NEW_FILE} DIRECTORY)
